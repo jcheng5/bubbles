e5d81ba1e70d6a34c44d7d5f82cedb374658229d
Joe Cheng <joe@rstudio.com>
2015-01-17 00:17:39 -0800

    Customize and document R functions

--- R/bubbles.R
+++ R/bubbles.R
@@ -1,15 +1,30 @@
-#' <Add Title>
-#'
-#' <Add Description>
-#'
+#' Bubble Chart
+#' 
+#' Creates a bubble chart.
+#' 
+#' @param value Numeric vector of values, used for sizing bubbles
+#' @param label Character vector of textual labels to use on bubbles
+#' @param tooltip Character vector of tooltip values, to be shown on hover
+#' @param color Character vector of color values, in \code{"#RRGGBB"} format
+#' @param width The width of the widget, or \code{NULL} for default.
+#' @param height The height of the widget, or \code{NULL} for default.
+#'   
+#' @return A widget object that can be printed at the console, outputted as part
+#'   of an R Markdown document, or used in a Shiny app with
+#'   \code{\link{renderBubbles}}.
+#'   
 #' @import htmlwidgets
-#'
+#'   
 #' @export
-bubbles <- function(message, width = NULL, height = NULL) {
+bubbles <- function(value, label, tooltip = NULL, color = NULL,
+  width = NULL, height = NULL) {
 
   # forward options using x
   x = list(
-    message = message
+    value = value,
+    label = label,
+    tooltip = tooltip,
+    color = color
   )
 
   # create widget
@@ -22,15 +37,21 @@ bubbles <- function(message, width = NULL, height = NULL) {
   )
 }
 
-#' Widget output function for use in Shiny
-#'
+#' Bubble chart functions for Shiny
+#' 
+#' @param outputId Shiny output ID for this bubble chart
+#' @param width,height Plot width/height. Must be a valid CSS unit (e.g. "100%",
+#'   "400px") or a number, which will be interpreted as pixels ("px").
+#' @param expr An expression that generates a \code{\link{bubbles}} widget.
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})?
+#'   
 #' @export
-bubblesOutput <- function(outputId, width = '100%', height = '400px'){
+bubblesOutput <- function(outputId, width = '100%', height = '600px'){
   shinyWidgetOutput(outputId, 'bubbles', width, height, package = 'bubbles')
 }
 
-#' Widget render function for use in Shiny
-#'
+#' @rdname bubblesOutput
 #' @export
 renderBubbles <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
